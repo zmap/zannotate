9c813149f4eec219291a00567e02ec4cc429586f
David Adrian <davidcadrian@gmail.com>
2020-03-25 17:34:28 -0400

    Add filter function for AS paths in ZRouting
    Add an example filter that cleans out internal BGP routes by making them
    look like a single origin AS.

---
+++ zrouting/routinglookup_test.go
@@ -0,0 +1,59 @@
+package zrouting
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+type pathFilterTest struct {
+	path     []uint32
+	filter   PathFilter
+	expected []uint32
+}
+
+// 64512 - 65534
+
+func TestIBGPFilter(t *testing.T) {
+	tests := []pathFilterTest{
+		pathFilterTest{
+			path:     []uint32{1, 2, 3, 4},
+			filter:   IdentityPathFilter,
+			expected: []uint32{1, 2, 3, 4},
+		},
+		pathFilterTest{
+			path:     []uint32{1, 65000, 38},
+			filter:   InternalBGPPathFilter(38),
+			expected: []uint32{1, 38},
+		},
+		pathFilterTest{
+			path:     []uint32{1, 65000},
+			filter:   InternalBGPPathFilter(38),
+			expected: []uint32{1, 38},
+		},
+		pathFilterTest{
+			path:     []uint32{1, 65000, 64512, 65118, 38, 27},
+			filter:   InternalBGPPathFilter(38),
+			expected: []uint32{1, 38, 27},
+		},
+		pathFilterTest{
+			path:     []uint32{1, 65000, 64512, 65118},
+			filter:   InternalBGPPathFilter(38),
+			expected: []uint32{1, 38},
+		},
+		pathFilterTest{
+			path:     []uint32{1, 2, 2, 64512, 64512, 38, 3, 3},
+			filter:   InternalBGPPathFilter(38),
+			expected: []uint32{1, 2, 2, 38, 3, 3},
+		},
+		pathFilterTest{
+			path:     []uint32{1, 2, 2, 64512, 64512, 3, 3},
+			filter:   InternalBGPPathFilter(38),
+			expected: []uint32{1, 2, 2, 38, 3, 3},
+		},
+	}
+	for _, test := range tests {
+		actual := test.filter(test.path)
+		assert.DeepEqual(t, test.expected, actual)
+	}
+}
