418e767ea8abe817502fdaa50f0e5f0fae22a252
phillip-stephens <phillip@cs.stanford.edu>
2025-06-06 15:08:05 -0700

    Add open-source license to headers of file, make license-check works now

--- .github/workflows/check_license.sh
+++ .github/workflows/check_license.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export NO_HEADERS_FOUND=$(grep -RiL --include="*.go" "ZDNS Copyright .* Regents of the University of Michigan" ./)
+export NO_HEADERS_FOUND=$(grep -RiL --include="*.go" "ZAnnotate Copyright .* Regents of the University of Michigan" ./)
 if test -n "$NO_HEADERS_FOUND"; then
   echo 'FOUND .go FILES WITHOUT HEADERS.'
   echo "$NO_HEADERS_FOUND"
